3ee0835a40458e215333dd6c6b84c66e28bb3327
Ben Greaves <bsgreaves@gmail.com>
2017-04-18 13:25:58 -0500

    Minor JS changes. Needs Readme

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-var UI = (function(oldUI) {
+var UI = (function(oldUI) { // IIFE structure so I can keep the data private
 
     var selectedCategory = "";
     var categories = [];
@@ -6,14 +6,14 @@ var UI = (function(oldUI) {
     var products = [];
     var JSONFiles = ["./json/categories.json", "./json/types.json", "./json/products.json"];
 
-    oldUI.loadUserSelection = function(userSelectedCategory) { // Primary function
+    oldUI.loadUserSelection = function(userSelectedCategory) { // Triggered by event listener
         selectedCategory = userSelectedCategory;
         Promise.all([UI.loadJSONFile(JSONFiles[0]), UI.loadJSONFile(JSONFiles[1]), UI.loadJSONFile(JSONFiles[2])])
             .then(function(data) {
                 categories = data[0].categories;
                 types = data[1].types;
                 products = data[2].products;
-                UI.buildDOMString();
+                UI.createDOMString();
             });
     };
     oldUI.loadJSONFile = function(filepath) { // Loads each JSON file
@@ -22,10 +22,10 @@ var UI = (function(oldUI) {
                 resolve(data);
             }).fail(function(error) {
                 reject(error);
-            })
-        })
+            });
+        });
     };
-    oldUI.buildDOMString = function() { //Builds the main string
+    oldUI.createDOMString = function() { // Builds the main string
         let cardCount = 0;
         let ops = ""; //outputstring
         categories.forEach(function(currCategory) {
@@ -48,35 +48,19 @@ var UI = (function(oldUI) {
                 });
             }
         });
+        if (cardCount % 3 !== 0) { // Closes out any row that wasn't closed automatically
+            ops += `</div>`;
+        }
         $("#output").html(ops);
     };
     oldUI.buildCard = function(currType, currProduct) { // Assists buildDOMString
-        let currCard = `<div class="col-md-4">`
-        currCard += `<h3>${currProduct.name}</h3>`
-        currCard += `<p>${currType.name}</p>`
-        currCard += `<p>${currProduct.description}</p>`
-        currCard += `</div>`
+        let currCard = `<div class="col-md-4">`;
+        currCard += `<h3>${currProduct.name}</h3>`;
+        currCard += `<p>${currType.name}</p>`;
+        currCard += `<p>${currProduct.description}</p>`;
+        currCard += `</div>`;
         return currCard;
     };
-
-    //  ATTEMPT to not hardcode promise.all
-
-    // oldUI.returnBlankPromiseArray = function(loadJSONFunction, filepathArray) {
-    // 	let blankPromiseArray = [];
-    // 	for (i=0; i < filepathArray.length; i++){
-    // 		blankPromiseArray.push(null);
-    // 	}
-    // 	blankPromiseArray.fill(loadJSONFunction, 0, filepathArray.length);
-    // 	return blankPromiseArray;
-    // }
-
-    // oldUI.executeJSON = function(loadJSONFunction, filepathArray) {
-    // 	let promiseArray = UI.returnBlankPromiseArray(loadJSONFunction, filepathArray);
-    // 	console.log("promisearray in executeJSON", promiseArray);
-    // 	return promiseArray.map(function(each, index){
-    // 		each(filepathArray[index]);
-    // 	});
-    // }
 
     $("#Fireworks, #Explosives").click(function(e) {
         UI.loadUserSelection(e.target.id);
